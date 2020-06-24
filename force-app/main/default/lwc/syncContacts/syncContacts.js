d81ac40f76132f79205a192010b0c0d9d2086105
Adrian <naikonpaul@gmail.com>
2020-06-24 21:19:31 +0200

    end

--- force-app/main/default/lwc/syncContacts/syncContacts.js
+++ force-app/main/default/lwc/syncContacts/syncContacts.js
@@ -1,6 +1,7 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getSyncContacts from "@salesforce/apex/SyncContactsController.getContactsFromOrgA";
+import batchContacts from "@salesforce/apex/SyncContactsController.sendContactsToOrgB";
 
 export default class SyncContacts extends LightningElement {
 
@@ -20,4 +21,21 @@ export default class SyncContacts extends LightningElement {
                 );
             });
     }
+    
+    handleClick1() {
+        batchContacts()
+            .then(result => {
+                console.log(result);
+            })
+            .catch(result => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error al batchear los contactos !',
+                        message: JSON.stringify(result),
+                        variant: 'error',
+                    }),
+                );
+            })
+    }
+
 }
